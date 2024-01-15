97f9273feee2238c8ba63468ad3d13b61afcde4f
Kartik Pontula <kartikpontula@gmail.com>
2024-01-16 01:17:42 +0530

    Bare-bones HTTP Golang server with Dockerfile

---
+++ A1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/home", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string {
+			"message": "Hello!",
+			"status": "successful",
+		})
+	})
+
+	e.GET("/heartbeat", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "5000"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
