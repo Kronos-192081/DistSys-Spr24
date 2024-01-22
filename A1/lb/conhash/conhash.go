4075d5e9efb7a2c6b6f9e7ffb3e118c1c5ad700d
Prakhar Singh <98411123+prakhar4660@users.noreply.github.com>
2024-01-22 21:48:44 +0530

    go program for consistent hashing

---
+++ A1/lb/conhash/conhash.go
@@ -0,0 +1,138 @@
+package conhash
+
+import (
+	"fmt"
+)
+
+
+// Node represents a node in the ConHash structure.
+type Node struct {
+	Occ  bool
+	Name string
+}
+
+// ConHash is a consistent hashing implementation.
+type ConHash struct {
+	HashD      []Node
+	Size		int
+	VirtServ   	int
+	Nserv       int
+	AllServers map[string]int
+}
+
+// NewConHash creates a new ConHash instance.
+func NewConHash(m, k int) *ConHash {
+	return &ConHash{
+		HashD:      make([]Node, m),
+		Size:       m,
+		VirtServ:   k,
+		Nserv:		0,
+		AllServers: make(map[string]int),
+	}
+}
+
+func (c *ConHash) getServHash(i, j int) int {
+	val := 1*i*i + 1*j*j + 2*j + 25
+	val %= int(c.Size)
+	return int(val)
+}
+
+func (c *ConHash) getCliHash(i int) int {
+	val := 1*i*i + 1*2*i + 17
+	val %= int(c.Size)
+	return int(val)
+}
+
+// Add adds servers to the consistent hash ring.
+func (c *ConHash) Add(ids []int, Names []string) int {
+	if len(ids) != len(Names) {
+		return 0
+	}
+
+	for _, name := range Names {
+		if _, ok := c.AllServers[name]; ok {
+			return 0
+		}
+	}
+	
+	if (c.Nserv+len(ids))*c.VirtServ >= c.Size {
+		return 0
+	}
+	c.Nserv += len(ids)
+
+	for i := 0; i < len(ids); i++ {
+		c.AllServers[Names[i]] = 1
+		for j := 0; j < c.VirtServ; j++ {
+			hash := c.getServHash(ids[i], j)
+			for c.HashD[hash].Occ {
+				hash = (hash + 1) % c.Size
+			}
+			c.HashD[hash] = Node{true, Names[i]}
+		}
+	}
+	return 1
+}
+
+// GetConfig prints the configuration of the consistent hash ring.
+func (c *ConHash) GetConfig() {
+	for i := 0; i < c.Size; i++ {
+		fmt.Printf("Index: %d Status: %t Server: %s\n", i, c.HashD[i].Occ, c.HashD[i].Name)
+	}
+}
+
+// AddServer adds a single server to the consistent hash ring.
+func (c *ConHash) AddServer(id int, Name string) int {
+	if _, ok := c.AllServers[Name]; ok {
+		fmt.Println("Same server name already exists")
+		return 0
+	}
+
+	if (c.Nserv+1)*c.VirtServ >= c.Size {
+		fmt.Println("Size limit exceeded")
+		return 0
+	}
+
+	c.Nserv++
+	c.AllServers[Name] = 1
+
+	for j := 0; j < c.VirtServ; j++ {
+		hash := c.getServHash(id, j)
+		for c.HashD[hash].Occ {
+			hash = (hash + 1) % c.Size
+		}
+		c.HashD[hash] = Node{true, Name}
+	}
+
+	return 1
+}
+
+// RemoveServer removes a server from the consistent hash ring.
+func (c *ConHash) RemoveServer(Name string) int {
+	if _, ok := c.AllServers[Name]; !ok {
+		return 0
+	}
+
+	for i := range c.HashD {
+		if c.HashD[i].Name == Name {
+			c.HashD[i] = Node{false, ""}
+		}
+	}
+	delete(c.AllServers, Name)
+	c.Nserv--
+	return 1
+}
+
+// GetServer returns the server for the given client ID.
+func (c *ConHash) GetServer(id int) string {
+	if c.Nserv == 0 {
+		return "No Server Allocable"
+	}
+	hash := c.getCliHash(id)
+	fmt.Println("hash for ", id, " --> ", hash)
+	hash = (hash + 1) % c.Size
+	for !c.HashD[hash].Occ {
+		hash = (hash + 1) % c.Size
+	}
+
+	return c.HashD[hash].Name
+}
